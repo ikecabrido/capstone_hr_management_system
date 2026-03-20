670a1c4b155caa85e5eb2194e8a12f09d0c9e650
monstrebor <crobertjanssen@gmail.com>
2026-03-20 21:53:12 +0800

    fixing

--- employee_portal/index.php
+++ employee_portal/index.php
@@ -1,10 +1,7 @@
 <?php
 session_start();
 
-require 'controllers/RequestTypeController.php';
-require 'controllers/EmployeePortalController.php';
-require 'controllers/RequestController.php';
-require 'controllers/LeaveRequestController.php';
+require 'app/controllers/EmployeePortalController.php';
 
 $url = $_GET['url'] ?? 'dashboard';
 
@@ -14,70 +11,9 @@ switch ($url) {
         (new EmployeePortalController)->index();
         break;
 
-    case 'request-types':
-        (new RequestTypeController)->index();
-        break;
-
-    case 'request-types-create':
-        (new RequestTypeController)->create();
-        break;
-
-    case 'request-types-update':
-        (new RequestTypeController)->update();
-        break;
-
-    case 'request-types-delete':
-        (new RequestTypeController)->delete();
-        break;
-
-    case 'request-index':
-        (new RequestController)->index();
-        break;
-
-    case 'request-create':
-        (new RequestController)->create();
-        break;
-
-    case 'request-status-update':
-        (new RequestController)->updateRequestStatus();
-        break;
-
-    case 'request-download':
-        (new RequestController)->download();
-        break;
-
-    case 'request-update-status':
-        (new RequestController)->updateRequestStatus();
-        break;
-
-    case 'request-delete':
-        (new RequestController)->delete();
-        break;
-
-    case 'leave-requests-index':
-        (new LeaveRequestController)->index();
-        break;
-
-    case 'employee-leave-requests-create':
-        (new LeaveRequestController)->createLeave();
-        break;
-
-    case 'leave-requests-create':
-        (new LeaveRequestController)->create();
-        break;
-
-    case 'leave-requests-update-status':
-        (new LeaveRequestController)->updateStatus();
-        break;
-
-    case 'leave-requests-delete':
-        (new LeaveRequestController)->delete();
-        break;
-
-
     default:
         $title = "Page Not Found";
-        $content = __DIR__ . '/views/error-content.php';
+        $content = __DIR__ . 'app/views/error-content.php';
         require __DIR__ . '/layout.php';
         break;
 }
