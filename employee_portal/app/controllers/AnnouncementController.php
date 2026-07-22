d8fc5bc5b024e248633f18611dea6c34521db0a8
monstrebor <crobertjanssen@gmail.com>
2026-07-22 23:57:04 +0800

    merge:

--- employee_portal/app/controllers/AnnouncementController.php
+++ employee_portal/app/controllers/AnnouncementController.php
@@ -1,31 +1,15 @@
 <?php
-
-// Load the model 
 require_once __DIR__ . '/../models/Announcement.php';
-
 class AnnouncementController
 {
-    // Instance of the model
     private $announcementModel;
-
-    public function __construct()
+        public function __construct()
     {
-        // Initialize the model so we can access data
         $this->announcementModel = new Announcement();
     }
-
-    /**
-     * Display the list of announcements
-     *
-     * - Fetches all announcements from the database
-     * - Passes data to the view
-     * - Loads the main announcements layout
-     */
     public function index()
     {
-        // Get all announcements
         $announcements = $this->announcementModel->all();
-
         $title = "Employee Announcements";
         $content = __DIR__ . '/../views/announcements/main-content.php';
         require __DIR__ . '/../views/employee-portal/index.php';
